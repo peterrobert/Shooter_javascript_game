ce3f60b90ac0c9c29dc293f99d9ac2674ba515c9
peterrobert <pwambui93@gmail.com>
2020-11-02 12:01:33 +0300

    created our classes

---
+++ src/SceneMainMenu.js
@@ -0,0 +1,10 @@
+class SceneMainMenu extends Phaser.Scene {
+    constructor() {
+      super({ key: "SceneMainMenu" });
+    }
+  
+    create() {
+      this.scene.start("SceneMain");
+    }
+  }
+  
