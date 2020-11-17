131b02dd84bf9835eb4aa5298dfa91ed3b0736c7
peterrobert <pwambui93@gmail.com>
2020-11-17 23:28:06 +0300

    save the score successfully

--- src/score.js
+++ src/score.js
@@ -1,15 +1,34 @@
 class ScoreSave {
+  constructor(_name, _score) {
+    this.name = _name;
+    this.score = _score;
+  }
 
-    constructor(_name, _score){
-        this.name = _name
-        this.score = _score
-    }
+  save() {
+    let _data = {
+      user: this.name,
+      score: this.score,
+    };
 
+    fetch(
+      "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/tHxVDyAAsZqSgm0dnTap/scores",
+      {
+        method: "POST",
+        body: JSON.stringify(_data),
+        headers: { "Content-type": "application/json; charset=UTF-8" },
+      }
+    )
+      .then((response) => response.json())
+      .then((json) => console.log(json));
+  }
 
-    save(){
-        console.log (this.name);
-        console.log(this.score);
-    }
+ leaderBoard () {
+    new Promise((resolve, reject) => {
+        this.save();
+        
+      })
+ }
+
 }
 
-export {ScoreSave}
+export { ScoreSave };
